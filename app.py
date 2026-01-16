fe6041bbc6c9a2bed338e4b32b6b5243cbc423ef
AmalFrancisOlakengil <francisamal030@gmail.com>
2026-01-16 20:54:37 +0530

    testing with medium.com

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
-url = "https://www.instagram.com/p/DSxQ85CCKJh/"
+url = "https://medium.com/tag/technology/recommended"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36"
@@ -12,13 +13,29 @@ response.raise_for_status()   # raises error if request failed
 
 html = response.text
 
-soup = BeautifulSoup(html, "lxml")
+# 2. Create the folder if it doesn't exist
+folder_name = "html_output"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
+# 3. Define the file path
+file_path = os.path.join(folder_name, "index.html")
+
+# 4. Save the variable into the file
+with open(file_path, "w") as file:
+    file.write(html)
+
+print(f"File saved successfully at: {file_path}")
+'''
+soup = BeautifulSoup(html, "lxml")
+print("_aagv" in html)
 div = soup.find("div", class_="_aagv")
 img = div.find("img")
 
 image_url = img.get("src") or img.get("data-src")
 
 print(image_url)
+'''
+
 
 
