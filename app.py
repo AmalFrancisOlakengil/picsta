1f03c0fb503858f71981096ce0ca40c63617569b
AmalFrancisOlakengil <francisamal030@gmail.com>
2026-01-18 18:35:13 +0530

    Figured out the html block for reviews, and scrapped successfully

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def get_amazon_product(url):
         print(f"Error: Received status code {response.status_code}")
         return None
 
+    #with open("output.html", "w", encoding="utf-8") as file:
+    # file.write(response.text)
+
     # Parse HTML
     soup = BeautifulSoup(response.content, "lxml")
 
@@ -24,6 +27,18 @@ def get_amazon_product(url):
         data['title'] = soup.find("span", attrs={"id": 'productTitle'}).get_text(strip=True)
         data['price'] = soup.find("span", attrs={"class": 'a-offscreen'}).get_text(strip=True)
         data['rating'] = soup.find("span", attrs={"class": 'a-icon-alt'}).get_text(strip=True)
+        review_elements = soup.find_all('li', {'data-hook': 'review'})
+        extracted_reviews = []
+        for review in review_elements:
+            review_item = {
+                'id': review.get('id'),
+                'title': review.find('a', {'data-hook': 'review-title'}).get_text(strip=True) if review.find('a', {'data-hook': 'review-title'}) else None,
+                'rating': review.find('i', {'data-hook': 'review-star-rating'}).get_text(strip=True) if review.find('i', {'data-hook': 'review-star-rating'}) else None,
+                'body': review.find('span', {'data-hook': 'review-body'}).get_text(strip=True) if review.find('span', {'data-hook': 'review-body'}) else None
+            }
+            extracted_reviews.append(review_item)
+            
+        data['reviews'] = extracted_reviews
     except AttributeError:
         print("Could not find some elements. Amazon might have blocked the request or changed layout.")
     
